adede4786ca0d8650734761f2d4272a5fa46aa34
Prashant Fule <fuleprashant2809@gmail.com>
2025-01-18 21:14:44 +0530

    add the latest model of user

---
+++ backend/model/user.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: function () {
+      return !this.googleId; // Required only if googleId is not present
+    },
+    select: false, // Exclude password from queries by default
+  },
+  googleId: {
+    type: String, // For Google OAuth users
+    unique: true,
+    sparse: true, // Allows null or missing values but enforces uniqueness if present
+  },
+  profilePicture: {
+    type: String, // URL for the user's profile pictureF
+    default: null,
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  emailVerificationOtp: {
+    type: String,
+    select: false,
+  },
+  verificationCodeExpire: {
+    type: Date,
+    select: false,
+  },
+  resetPasswordOtp: {
+    type: String,
+    select: false,
+  },
+  resetPasswordOtpExpire: {
+    type: Date,
+    select: false,
+  },
+});
+
+// Create the model
+const User = mongoose.model("User", userSchema);
+
+export default User;
