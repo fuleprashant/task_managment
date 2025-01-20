bf5446f0f7ee12e81c218ca5b99e11dba975d2c9
Prashant Fule <bhavik.shah@lancerworldwide.com>
2025-01-20 16:52:19 +0530

    add the conficts data

---
+++ backend/routes/user.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+import {
+  forget_password,
+  login,
+  logout,
+  reset_password,
+  signUp,
+  verifyOtp,
+} from "../controller/user.controllers.js";
+
+const router = express.Router();
+
+router.post("/signup", signUp);
+router.post("/verifyotp", verifyOtp);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/forgetpassword", forget_password);
+router.post("/resetpassword", reset_password);
+
+export default router     
