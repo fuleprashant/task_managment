8e9b34ff332718fb1f6a4609f33e2a023024cb41
Prashant Fule <bhavik.shah@lancerworldwide.com>
2025-01-30 10:54:50 +0530

    loginuser

--- frontend/src/protectedroutes/LoginUser.jsx
+++ frontend/src/protectedroutes/LoginUser.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const LoginUser = ({ children }) => {
+  console.log(children);
   const token = localStorage.getItem("token");
 
   if (token) {
